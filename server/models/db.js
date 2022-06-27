1e19dd0ade56e3cc2174a967888a810447641c1b
Quang Ninh <38124861+qninhks123@users.noreply.github.com>
2022-06-27 15:23:27 +0700

    Update db.js

--- server/models/db.js
+++ server/models/db.js
@@ -1,6 +1,8 @@
 var sql =  require("sequelize");
 var db_host,db_user,db_name,db_password;
 
+console.log("lmao");
+
 if(process.env.PORT!=undefined){
     db_host = 'www.db4free.net';
     db_user = 'qninhks_123';
