27e50d75409db6fd3c42818805d32270c54678c1
qninhdt <qndt123@gmail.com>
2023-05-17 22:46:07 +0700

    I miss u

--- server/models/db.js
+++ server/models/db.js
@@ -1,38 +1,38 @@
-var sql =  require("sequelize");
-var db_host,db_user,db_name,db_password;
+var sql = require("sequelize");
+var db_host, db_user, db_name, db_password;
 
-console.log("lmao_"); 
+console.log("lmao_");
 
-if(process.env.PORT!=undefined){
-    db_host = 'www.db4free.net';
-    db_user = 'qninhks_123';
-    db_name = 'qninh_123';
-    db_password = 'Qninhks123@';
-} else {
-    db_host = 'localhost';
-    db_user = 'root';
-    db_name = 'server';
-    db_password = '12345';
-}
+// if(process.env.PORT!=undefined){
+db_host = "sql12.freemysqlhosting.net";
+db_user = "sql12619044";
+db_name = "sql12619044";
+db_password = "2uEHmQ67tl";
+// } else {
+//     db_host = 'localhost';
+//     db_user = 'root';
+//     db_name = 'server';
+//     db_password = '12345';
+// }
 
-var db  =  new sql(db_name,db_user,db_password,{
-    host: db_host,
-    dialect: 'mysql',
-    logging: false,
-    operatorsAliases : false,
-    define : { 
-		freezeTableName : true, 
-        timestamps: true,
-        collate: 'utf8mb4_unicode_ci'        
-    },
-    
-    pool: {
-        max: 10,
-        min: 0,
-        idle: 50000,
-        acquire: 20000
-    }
-}); 
+var db = new sql(db_name, db_user, db_password, {
+  host: db_host,
+  dialect: "mysql",
+  logging: false,
+  operatorsAliases: false,
+  define: {
+    freezeTableName: true,
+    timestamps: true,
+    collate: "utf8mb4_unicode_ci",
+  },
+
+  pool: {
+    max: 10,
+    min: 0,
+    idle: 50000,
+    acquire: 20000,
+  },
+});
 
 // db.authenticate()
-module.exports = {db,sql};
+module.exports = { db, sql };
--- server/models/total.js
+++ server/models/total.js
@@ -1,10 +1,10 @@
-var { db,sql } = require("./db");
+var { db, sql } = require("./db");
 
-module.exports = db.define("total",{
-    testcode : sql.STRING,
-    code     : sql.STRING,
-    total    : {
-        type : sql.INTEGER,
-        defaultValue : 0,
-    }
+module.exports = db.define("total", {
+  testcode: sql.STRING,
+  code: sql.STRING,
+  total: {
+    type: sql.INTEGER,
+    defaultValue: 0,
+  },
 });
